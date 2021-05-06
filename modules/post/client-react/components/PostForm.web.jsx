87ebbc9d0729c90102833a69471d0dba03169283
keyurmeandev <keyur.rathod84@gmail.com>
2021-05-06 22:49:02 +0530

    Update PostForm.web.jsx

--- modules/post/client-react/components/PostForm.web.jsx
+++ modules/post/client-react/components/PostForm.web.jsx
@@ -4,31 +4,33 @@ import { withFormik } from 'formik';
 import { translate } from '@gqlapp/i18n-client-react';
 import { FieldAdapter as Field } from '@gqlapp/forms-client-react';
 import { required, validate } from '@gqlapp/validation-common-react';
-import { Form, RenderField, Button, Media } from '@gqlapp/look-client-react';
+import { Form, RenderField, Media, Button, Table, Alert } from '@gqlapp/look-client-react';
+import { FormGroup, Label, Input } from 'reactstrap';
 
 const postFormSchema = {
   title: [required],
   content: [required],
-  image: [required]
+  image: [required],
+  file: [required]
 };
 
-const PostForm = ({ values, handleSubmit, submitting, t }) => {
-  const [image, setImage] = useState();
-  const handleImage = function (value, ev) {
+const PostForm = ({ values, handleSubmit, setFieldValue, submitting, t }) => {
+  const [image, setImage] = useState(values.image ? `/public/post/${values.image}` : null);
+
+  const handleImage = function(ev) {
     ev.preventDefault();
-    console.log(`PostForm: handle image running`, value);
     // Assuming only image
     var file = ev.target.files[0];
+    setFieldValue(`file`, file);
     var reader = new FileReader();
-    var url = reader.readAsDataURL(file);
+    reader.readAsDataURL(file);
     reader.onloadend = () => {
       let image = [reader.result];
       setImage(image);
     };
-    console.log(url); // Would see a path?
-    // setImage(e.target.files[0]);
   };
 
+  console.log(values,image);
   return (
     <Form name="post" onSubmit={handleSubmit}>
       <Field name="title" component={RenderField} type="text" label={t('post.field.title')} value={values.title} />
@@ -39,21 +41,14 @@ const PostForm = ({ values, handleSubmit, submitting, t }) => {
         label={t('post.field.content')}
         value={values.content}
       />
-      <Media>
+      <div style={{ height: '200px' }}>
+        <Media object data-src={image} src={image} alt="Generic" style={{ height: '100%' }} />
+      </div>
 
-        <Media object data-src={image} src={image} alt="Generic" />
-
-      </Media>
-      <Field
-        type="file"
-        name="Image"
-        component={RenderField}
-
-        label={t('post.field.image')}
-        // value={values.image}
-        onChange={handleImage}
-      />
-
+      <FormGroup>
+        <Label for="exampleFile">Image</Label>
+        <Input type="file" accept="image/*" name="image" id="image" onChange={handleImage} value={values.image} />
+      </FormGroup>
       <Button color="primary" type="submit" disabled={submitting}>
         {t('post.btn.submit')}
       </Button>
@@ -63,7 +58,7 @@ const PostForm = ({ values, handleSubmit, submitting, t }) => {
 
 PostForm.propTypes = {
   handleSubmit: PropTypes.func,
-  //handleImage: PropTypes.func,
+  setFieldValue: PropTypes.func,
   onSubmit: PropTypes.func,
   submitting: PropTypes.bool,
   values: PropTypes.object,
@@ -72,19 +67,23 @@ PostForm.propTypes = {
 };
 
 const PostFormWithFormik = withFormik({
+  initialValues: { file: null },
   mapPropsToValues: props => ({
     title: props.post && props.post.title,
     content: props.post && props.post.content,
-    image: props.image && props.post.image
+    image: props.image && props.post.image,
+    file: null
   }),
-  validate: values => validate(values, postFormSchema),
+  validate: values => {
+    validate(values, postFormSchema);
+  },
   handleSubmit(
     values,
     {
       props: { onSubmit }
     }
   ) {
-    onSubmit(values);
+    onSubmit({ title: values.title, content: values.content, image: values.file });
   },
   enableReinitialize: true,
   displayName: 'PostForm' // helps with React DevTools
